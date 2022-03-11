33c7dedbc799f35a2a07b4530d3a8e39552120d8
claudiaggg <claudia.garcia.gil@estudiantat.upc.edu>
2022-03-11 10:01:18 +0100

    Metodo suma

--- src/GestorFiguras.java
+++ src/GestorFiguras.java
@@ -1,4 +1,15 @@
 public abstract class GestorFiguras {
 
     public abstract double area();
+
+    public double Suma(GestorFiguras[] v)
+    {
+        double suma=0;
+        for (GestorFiguras f: v)
+        {
+            suma+=f.area();
+        }
+        return suma;
+
+    }
 }
